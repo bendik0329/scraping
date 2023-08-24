a292b90dc277d8d7c9a7b3ec1a318da3fcce89cd
CandyDEV1014 <lancakster512@gmail.com>
2023-08-24 23:49:43 +0300

    update script

--- index.php
+++ index.php
@@ -42,9 +42,13 @@ $htmlDomParser = HtmlDomParser::str_get_html($htmlContent);
 $currentPageNum = $htmlDomParser->findOne("a[aria-pressed=\"true\"]")->text;
 $nextPageNum = intval($currentPageNum) + 1;
 
+$nextPageLink = $driver->findElement(WebDriverBy::cssSelector("a[@title='Page 2']"));
+
 print_r("current page->" . $currentPageNum);
 print_r("\n");
 print_r("next page->" . $nextPageNum);
+print_r("\n");
+print_r($nextPageLink);
 print_r("\n");
 exit();
 

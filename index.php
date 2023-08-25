45b832a79d27563f144e5aa8c2b8431ba5da028d
CandyDEV1014 <lancakster512@gmail.com>
2023-08-25 15:10:26 +0300

    update script

--- index.php
+++ index.php
@@ -32,166 +32,80 @@ $host = 'http://localhost:4444/wd/hub';
 $capabilities = \Facebook\WebDriver\Remote\DesiredCapabilities::chrome();
 $capabilities->setCapability('goog:chromeOptions', ['args' => ["--headless", "--user-agent=" . USER_AGENT]]);
 $driver = RemoteWebDriver::create($host, $capabilities);
-$driver->get('https://api.scrapingdog.com/scrape?api_key=64e5b95985a16a20b0fdf02c&url=https://www.zillow.com/in/foreclosures/');
 
 $result = [];
+$url = "https://www.zillow.com/in/foreclosures";
+$driver->get('https://api.scrapingdog.com/scrape?api_key=64e4c5478d07b1208ead57b8&url=' . $url . "&dynamic=false");
 
-while (true) {
-  $html = $driver->findElement(WebDriverBy::tagName('html'));
-  $html->sendKeys(WebDriverKeys::END);
-  sleep(5);
+$totalCount = $driver->findElement(WebDriverBy::cssSelector(".search-subtitle span.result-count"))->getText();
+$pattern = '/\d+/';
 
-  $propertyElements = $driver->findElements(WebDriverBy::cssSelector("#grid-search-results > ul > li > div > div > article.property-card"));
-  if (count($propertyElements) > 0) {
-    foreach ($propertyElements as $propertyElement) {
-      $zpid = str_replace("zpid_", "", $propertyElement->getAttribute("id"));
-      $url = $propertyElement->findElement(WebDriverBy::cssSelector("div.property-card-data > a"))->getAttribute("href");
-      $address = $propertyElement->findElement(WebDriverBy::cssSelector("div.property-card-data > a > address"))->getText();
-      $price = $propertyElement->findElement(WebDriverBy::cssSelector("div.property-card-data span.PropertyCardWrapper__StyledPriceLine-srp__sc-16e8gqd-1"))->getText();
-      $beds = $propertyElement->findElement(WebDriverBy::cssSelector("div.property-card-data div.StyledPropertyCardDataArea-c11n-8-84-3__sc-yipmu-0.dbDWjx > ul > li:nth-child(1) > b"))->getText();
-      $baths = $propertyElement->findElement(WebDriverBy::cssSelector("div.property-card-data div.StyledPropertyCardDataArea-c11n-8-84-3__sc-yipmu-0.dbDWjx > ul > li:nth-child(2) > b"))->getText();
+preg_match('/\d+/', $totalCount, $matches);
 
-      $imgList = [];
-      // $imgFolder = __DIR__ . '/download/images/' . $zpid;
-      // if (!file_exists($imgFolder)) {
-      //   mkdir($imgFolder, 0777, true);
-      // }
+if (isset($matches[0])) {
+  $totalCount = intval($matches[0]);
+  $itemsPerPage = 41;
+  $currentPage = 1;
+  $maxPage = ceil($totalCount / $itemsPerPage);
 
-      $imgElements = $propertyElement->findElements(WebDriverBy::cssSelector("div.StyledPropertyCardPhoto-c11n-8-84-3__sc-ormo34-0.dGCVxQ.StyledPropertyCardPhoto-srp__sc-1gxvsd7-0"));
-      foreach ($imgElements as $imgElement) {
-        $imgUrl = $imgElement->findElement(WebDriverBy::cssSelector("img.Image-c11n-8-84-3__sc-1rtmhsc-0"))->getAttribute("src");
-        // $imgPath = $imgFolder . "/" . basename($imgUrl);
-        // $imgData = file_get_contents($imgUrl);
-        // if ($imgData !== false && !file_exists($imgPath)) {
-        //   file_put_contents($imgPath, $imgData);
+  var_dump($totalCount);
+  var_dump($maxPage);
+
+  while ($currentPage <= $maxPage) {
+    $pageUrl = "https://api.scrapingdog.com/scrape?api_key=64e4c5478d07b1208ead57b8&url=" . $url . "/" . $currentPage . "_p/";
+    $driver->executeScript("window.location.href = '$pageUrl';");
+    sleep(5);
+
+    $html = $driver->findElement(WebDriverBy::tagName('html'));
+    $html->sendKeys(WebDriverKeys::END);
+    sleep(5);
+
+    $propertyElements = $driver->findElements(WebDriverBy::cssSelector("#grid-search-results > ul > li > div > div > article.property-card"));
+    if (count($propertyElements) > 0) {
+      foreach ($propertyElements as $propertyElement) {
+        $zpid = str_replace("zpid_", "", $propertyElement->getAttribute("id"));
+        $url = $propertyElement->findElement(WebDriverBy::cssSelector("div.property-card-data > a"))->getAttribute("href");
+        $address = $propertyElement->findElement(WebDriverBy::cssSelector("div.property-card-data > a > address"))->getText();
+        $price = $propertyElement->findElement(WebDriverBy::cssSelector("div.property-card-data span.PropertyCardWrapper__StyledPriceLine-srp__sc-16e8gqd-1"))->getText();
+        $beds = $propertyElement->findElement(WebDriverBy::cssSelector("div.property-card-data div.StyledPropertyCardDataArea-c11n-8-84-3__sc-yipmu-0.dbDWjx > ul > li:nth-child(1) > b"))->getText();
+        $baths = $propertyElement->findElement(WebDriverBy::cssSelector("div.property-card-data div.StyledPropertyCardDataArea-c11n-8-84-3__sc-yipmu-0.dbDWjx > ul > li:nth-child(2) > b"))->getText();
+
+        $imgList = [];
+        // $imgFolder = __DIR__ . '/download/images/' . $zpid;
+        // if (!file_exists($imgFolder)) {
+        //   mkdir($imgFolder, 0777, true);
         // }
-        $imgList[] = $imgUrl;
+
+        $imgElements = $propertyElement->findElements(WebDriverBy::cssSelector("div.StyledPropertyCardPhoto-c11n-8-84-3__sc-ormo34-0.dGCVxQ.StyledPropertyCardPhoto-srp__sc-1gxvsd7-0"));
+        foreach ($imgElements as $imgElement) {
+          $imgUrl = $imgElement->findElement(WebDriverBy::cssSelector("img.Image-c11n-8-84-3__sc-1rtmhsc-0"))->getAttribute("src");
+          // $imgPath = $imgFolder . "/" . basename($imgUrl);
+          // $imgData = file_get_contents($imgUrl);
+          // if ($imgData !== false && !file_exists($imgPath)) {
+          //   file_put_contents($imgPath, $imgData);
+          // }
+          $imgList[] = $imgUrl;
+        }
+
+        $result[] = array(
+          "zpid" => $zpid,
+          "url" => $url,
+          "address" => $address,
+          "price" => $price,
+          "beds" => $beds,
+          "baths" => $baths,
+          "images" => $imgList,
+        );
       }
-
-      $result[] = array(
-        "zpid" => $zpid,
-        "url" => $url,
-        "address" => $address,
-        "price" => $price,
-        "beds" => $beds,
-        "baths" => $baths,
-        "images" => $imgList,
-      );
     }
-  }
 
-  $paginationElements = $driver->findElements(WebDriverBy::cssSelector("li.PaginationNumberItem-c11n-8-84-3__sc-bnmlxt-0.cA-Ddyj"));
-  if (count($paginationElements) > 0) {
-    try {
-      $currentPageNum = $driver->findElement(WebDriverBy::cssSelector("a[aria-pressed=\"true\"]"))->getText();
-      $nextPageNum = intval($currentPageNum) + 1;
-      $nextPageLink = $driver->findElement(WebDriverBy::cssSelector("a[title=\"Page " . strval($nextPageNum) . "\"]"));
-      $action = new WebDriverActions($driver);
-      $action->click($nextPageLink)->perform();
+    $currentUrl = $driver->getCurrentURL();
+    echo $currentUrl;
 
-      $wait = new WebDriverWait($driver, 10); // Maximum wait time of 10 seconds
-      $wait->until(function ($driver) {
-        return count($driver->findElements(WebDriverBy::className('list-loading-message-cover'))) === 0;
-      });
-
-      print_r($currentPageNum);
-      print_r($nextPageNum);
-    } catch (NoSuchElementException $e) {
-      print_r("pagination ended");
-      break;
-    }
-  } else {
-    print_r("pagination elements not exists");
-    break;
+    $currentPage++;
   }
 }
 
-$driver->close();
 echo json_encode($result);
+$driver->close();
 exit();
-
-$result = [];
-$currentPage = 1;
-$maxPage = 4;
-
-while ($currentPage <= $maxPage) {
-  $html = $driver->findElement(WebDriverBy::tagName('html'));
-  $html->sendKeys(WebDriverKeys::END);
-  sleep(5);
-
-  $propertyElements = $driver->findElements(WebDriverBy::cssSelector("#grid-search-results > ul > li > div > div > article.property-card"));
-  if (count($propertyElements) > 0) {
-    foreach ($propertyElements as $propertyElement) {
-      $zpid = str_replace("zpid_", "", $propertyElement->getAttribute("id"));
-      $url = $propertyElement->findElement(WebDriverBy::cssSelector("div.property-card-data > a"))->getAttribute("href");
-      $address = $propertyElement->findElement(WebDriverBy::cssSelector("div.property-card-data > a > address"))->getText();
-      $price = $propertyElement->findElement(WebDriverBy::cssSelector("div.property-card-data span.PropertyCardWrapper__StyledPriceLine-srp__sc-16e8gqd-1"))->getText();
-      $beds = $propertyElement->findElement(WebDriverBy::cssSelector("div.property-card-data div.StyledPropertyCardDataArea-c11n-8-84-3__sc-yipmu-0.dbDWjx > ul > li:nth-child(1) > b"))->getText();
-      $baths = $propertyElement->findElement(WebDriverBy::cssSelector("div.property-card-data div.StyledPropertyCardDataArea-c11n-8-84-3__sc-yipmu-0.dbDWjx > ul > li:nth-child(2) > b"))->getText();
-
-      $imgList = [];
-      // $imgFolder = __DIR__ . '/download/images/' . $zpid;
-      // if (!file_exists($imgFolder)) {
-      //   mkdir($imgFolder, 0777, true);
-      // }
-
-      $imgElements = $propertyElement->findElements(WebDriverBy::cssSelector("div.StyledPropertyCardPhoto-c11n-8-84-3__sc-ormo34-0.dGCVxQ.StyledPropertyCardPhoto-srp__sc-1gxvsd7-0"));
-      foreach ($imgElements as $imgElement) {
-        $imgUrl = $imgElement->findElement(WebDriverBy::cssSelector("img.Image-c11n-8-84-3__sc-1rtmhsc-0"))->getAttribute("src");
-        // $imgPath = $imgFolder . "/" . basename($imgUrl);
-        // $imgData = file_get_contents($imgUrl);
-        // if ($imgData !== false && !file_exists($imgPath)) {
-        //   file_put_contents($imgPath, $imgData);
-        // }
-        $imgList[] = $imgUrl;
-      }
-
-      $result[] = array(
-        "zpid" => $zpid,
-        "url" => $url,
-        "address" => $address,
-        "price" => $price,
-        "beds" => $beds,
-        "baths" => $baths,
-        "images" => $imgList,
-      );
-    }
-  }
-
-  $currentPage++;
-  $paginationElements = $driver->findElements(WebDriverBy::cssSelector("li.PaginationNumberItem-c11n-8-84-3__sc-bnmlxt-0.cA-Ddyj"));
-  if (count($paginationElements) > 0) {
-    try {
-      $currentPageNum = $driver->findElement(WebDriverBy::cssSelector("a[aria-pressed=\"true\"]"))->getText();
-      $nextPageNum = intval($currentPageNum) + 1;
-      $nextPageLink = $driver->findElement(WebDriverBy::cssSelector("a[title=\"Page " . strval($nextPageNum) . "\"]"));
-      $action = new WebDriverActions($driver);
-      $action->click($nextPageLink)->perform();
-      sleep(5);
-    } catch (NoSuchElementException $e) {
-      break;
-    }
-  } else {
-    break;
-  }
-
-
-  // $paginationElements = $html->findElements(WebDriverBy::cssSelector("li.PaginationNumberItem-c11n-8-84-3__sc-bnmlxt-0.cA-Ddyj"));
-  // if (count($paginationElements) > 0) {
-  //   try {
-  //     $currentPageNum = $html->findElement(WebDriverBy::cssSelector("a[aria-pressed=\"true\"]"))->getText();
-  //     $nextPageNum = intval($currentPageNum) + 1;
-  //     $nextPageLink = $driver->findElement(WebDriverBy::cssSelector("a[title=\"Page " . strval($nextPageNum) . "\"]"));
-  //     $action = new WebDriverActions($driver);
-  //     $action->click($nextPageLink)->perform();
-  //     sleep(5);
-  //   } catch (NoSuchElementException $e) {
-  //     break;
-  //   }
-  // } else {
-  //   break;
-  // }
-}
-
-$driver->close();
-echo json_encode($result);

a7de54863ee999c57c68313f17749af51873e117
CandyDEV1014 <lancakster512@gmail.com>
2023-08-25 17:27:28 +0300

    update script

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ if (!$db->connect($host, $username, $password, $dbname)) {
 // Set up Selenium WebDriver
 $host = 'http://localhost:4444/wd/hub';
 $capabilities = \Facebook\WebDriver\Remote\DesiredCapabilities::chrome();
-$capabilities->setCapability('goog:chromeOptions', ['args' => ["--headless", "--user-agent=" . USER_AGENT]]);
+$capabilities->setCapability('goog:chromeOptions', ['args' => ["--user-agent=" . USER_AGENT]]);
 $driver = RemoteWebDriver::create($host, $capabilities);
 
 $result = [];
@@ -58,54 +58,56 @@ if (isset($matches[0])) {
       $pageUrl = 'https://api.scrapingdog.com/scrape?api_key=64e4c5478d07b1208ead57b8&url=https://www.zillow.com/in/foreclosures/' . strval($currentPage) . '_p/&dynamic=false';
     }
 
+    $driver->get($pageUrl);
+
     // $driver->executeScript("window.location.href = '$pageUrl';");
     // $wait = new WebDriverWait($driver, 10); // Maximum wait time in seconds
     // $wait->until(WebDriverExpectedCondition::urlContains($pageUrl));
-    // sleep(5);
+    sleep(5);
 
-    // $html = $driver->findElement(WebDriverBy::tagName('html'));
-    // $html->sendKeys(WebDriverKeys::END);
-    // sleep(5);
+    $html = $driver->findElement(WebDriverBy::tagName('html'));
+    $html->sendKeys(WebDriverKeys::END);
+    sleep(5);
     // $wait->until(WebDriverExpectedCondition::elementToBeClickable(WebDriverBy::tagName('html')));
 
-    // $propertyElements = $driver->findElements(WebDriverBy::cssSelector("#grid-search-results > ul > li > div > div > article.property-card"));
-    // if (count($propertyElements) > 0) {
-    //   foreach ($propertyElements as $propertyElement) {
-    //     $zpid = str_replace("zpid_", "", $propertyElement->getAttribute("id"));
-    //     $url = $propertyElement->findElement(WebDriverBy::cssSelector("div.property-card-data > a"))->getAttribute("href");
-    //     $address = $propertyElement->findElement(WebDriverBy::cssSelector("div.property-card-data > a > address"))->getText();
-    //     $price = $propertyElement->findElement(WebDriverBy::cssSelector("div.property-card-data span.PropertyCardWrapper__StyledPriceLine-srp__sc-16e8gqd-1"))->getText();
-    //     $beds = $propertyElement->findElement(WebDriverBy::cssSelector("div.property-card-data div.StyledPropertyCardDataArea-c11n-8-84-3__sc-yipmu-0.dbDWjx > ul > li:nth-child(1) > b"))->getText();
-    //     $baths = $propertyElement->findElement(WebDriverBy::cssSelector("div.property-card-data div.StyledPropertyCardDataArea-c11n-8-84-3__sc-yipmu-0.dbDWjx > ul > li:nth-child(2) > b"))->getText();
+    $propertyElements = $driver->findElements(WebDriverBy::cssSelector("#grid-search-results > ul > li > div > div > article.property-card"));
+    if (count($propertyElements) > 0) {
+      foreach ($propertyElements as $propertyElement) {
+        $zpid = str_replace("zpid_", "", $propertyElement->getAttribute("id"));
+        $url = $propertyElement->findElement(WebDriverBy::cssSelector("div.property-card-data > a"))->getAttribute("href");
+        $address = $propertyElement->findElement(WebDriverBy::cssSelector("div.property-card-data > a > address"))->getText();
+        $price = $propertyElement->findElement(WebDriverBy::cssSelector("div.property-card-data span.PropertyCardWrapper__StyledPriceLine-srp__sc-16e8gqd-1"))->getText();
+        // $beds = $propertyElement->findElement(WebDriverBy::cssSelector("div.property-card-data div.StyledPropertyCardDataArea-c11n-8-84-3__sc-yipmu-0.dbDWjx > ul > li:nth-child(1) > b"))->getText();
+        // $baths = $propertyElement->findElement(WebDriverBy::cssSelector("div.property-card-data div.StyledPropertyCardDataArea-c11n-8-84-3__sc-yipmu-0.dbDWjx > ul > li:nth-child(2) > b"))->getText();
 
-    //     $imgList = [];
-    //     // $imgFolder = __DIR__ . '/download/images/' . $zpid;
-    //     // if (!file_exists($imgFolder)) {
-    //     //   mkdir($imgFolder, 0777, true);
-    //     // }
+        $imgList = [];
+        // $imgFolder = __DIR__ . '/download/images/' . $zpid;
+        // if (!file_exists($imgFolder)) {
+        //   mkdir($imgFolder, 0777, true);
+        // }
 
-    //     $imgElements = $propertyElement->findElements(WebDriverBy::cssSelector("div.StyledPropertyCardPhoto-c11n-8-84-3__sc-ormo34-0.dGCVxQ.StyledPropertyCardPhoto-srp__sc-1gxvsd7-0"));
-    //     foreach ($imgElements as $imgElement) {
-    //       $imgUrl = $imgElement->findElement(WebDriverBy::cssSelector("img.Image-c11n-8-84-3__sc-1rtmhsc-0"))->getAttribute("src");
-    //       // $imgPath = $imgFolder . "/" . basename($imgUrl);
-    //       // $imgData = file_get_contents($imgUrl);
-    //       // if ($imgData !== false && !file_exists($imgPath)) {
-    //       //   file_put_contents($imgPath, $imgData);
-    //       // }
-    //       $imgList[] = $imgUrl;
-    //     }
+        $imgElements = $propertyElement->findElements(WebDriverBy::cssSelector("div.StyledPropertyCardPhoto-c11n-8-84-3__sc-ormo34-0.dGCVxQ.StyledPropertyCardPhoto-srp__sc-1gxvsd7-0"));
+        foreach ($imgElements as $imgElement) {
+          $imgUrl = $imgElement->findElement(WebDriverBy::cssSelector("img.Image-c11n-8-84-3__sc-1rtmhsc-0"))->getAttribute("src");
+          // $imgPath = $imgFolder . "/" . basename($imgUrl);
+          // $imgData = file_get_contents($imgUrl);
+          // if ($imgData !== false && !file_exists($imgPath)) {
+          //   file_put_contents($imgPath, $imgData);
+          // }
+          $imgList[] = $imgUrl;
+        }
 
-    //     $result[] = array(
-    //       "zpid" => $zpid,
-    //       "url" => $url,
-    //       "address" => $address,
-    //       "price" => $price,
-    //       "beds" => $beds,
-    //       "baths" => $baths,
-    //       "images" => $imgList,
-    //     );
-    //   }
-    // }
+        $result[] = array(
+          "zpid" => $zpid,
+          "url" => $url,
+          "address" => $address,
+          "price" => $price,
+          // "beds" => $beds,
+          // "baths" => $baths,
+          "images" => $imgList,
+        );
+      }
+    }
 
     print_r($pageUrl);
     $currentPage++;
@@ -113,5 +115,5 @@ if (isset($matches[0])) {
 }
 
 echo json_encode($result);
-$driver->close();
+// $driver->close();
 exit();

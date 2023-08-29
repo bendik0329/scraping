fe14c02a39c480a7508bc121deb0c18d147f1896
CandyDEV1014 <lancakster512@gmail.com>
2023-08-29 18:58:31 +0300

    update script

--- index.php
+++ index.php
@@ -381,7 +381,7 @@ try {
             if (count($houseElements) > 0) {
               foreach ($houseElements as $houseElement) {
                 try {
-                  $svgElement = $houseElement->findElement(WebDriverBy::cssSelector("svg"))->getAttribute("viewBox");
+                  $svgElement = $houseElement->findElement(WebDriverBy::cssSelector("svg > title"))->getText();
                   // $title = $houseElement->findElement(WebDriverBy::tagName("title"))->getText();
                   // $value = $houseElement->findElement(WebDriverBy::cssSelector("span.Text-c11n-8-84-3__sc-aiai24-0.dpf__sc-2arhs5-3.hrfydd.kOlNqB"))->getText();
                   // print_r($title);

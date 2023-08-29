6a40bab23ab8d0a1138b642bcce6e171fec3d7e2
CandyDEV1014 <lancakster512@gmail.com>
2023-08-29 22:20:16 +0300

    update script

--- utils/scraping.php
+++ utils/scraping.php
@@ -2,11 +2,128 @@
 
 require_once  __DIR__ . '/database.php';
 
+use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\Exception\NoSuchElementException;
+use Facebook\WebDriver\WebDriver;
+use Facebook\WebDriver\WebDriverKeys;
 
-function scrapeProperties($propertyElements) {
+function _init()
+{
+  global $db, $conn;
+  
+  // check properties table
+  $dropPropertiesSql = "DROP TABLE IF EXISTS properties";
+
+  if ($db->query($dropPropertiesSql) === TRUE) {
+    $createPropertiesSql = "CREATE TABLE IF NOT EXISTS properties (
+    `id` INT ( 6 ) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `zpid` VARCHAR ( 255 ) NOT NULL UNIQUE,
+    `url` VARCHAR ( 255 ) NOT NULL,
+    `address` VARCHAR ( 255 ),
+    `price` VARCHAR ( 255 ),
+    `beds` VARCHAR ( 255 ),
+    `baths` VARCHAR ( 255 ),
+    `images` TEXT,
+    `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP 
+  )";
+
+    if ($db->query($createPropertiesSql) === TRUE) {
+      echo "Table properties created successfully \n";
+    } else {
+      echo "Error creating properties table: " . $conn->error . "\n";
+    }
+  } else {
+    echo "Error dropping properties table: " . $conn->error . "\n";
+  }
+
+  // check images table
+  $dropImagesSql = "DROP TABLE IF EXISTS images";
+
+  if ($db->query($dropImagesSql) === TRUE) {
+    $imagesSql = "CREATE TABLE IF NOT EXISTS images (
+    `id` INT ( 6 ) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `zpid` VARCHAR ( 255 ) NOT NULL,
+    `url` VARCHAR ( 255 ) NOT NULL,
+    `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP 
+  )";
+
+    if ($db->query($imagesSql) === TRUE) {
+      echo "Table images created successfully \n";
+    } else {
+      echo "Error creating images table: " . $conn->error . "\n";
+    }
+  } else {
+    echo "Error dropping images table: " . $conn->error . "\n";
+  }
+
+  // check price_history table
+  $dropPriceHistorySql = "DROP TABLE IF EXISTS price_history";
+
+  if ($db->query($dropPriceHistorySql) === TRUE) {
+    $priceHistorySql = "CREATE TABLE IF NOT EXISTS price_history (
+    `id` INT ( 6 ) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `zpid` VARCHAR ( 255 ) NOT NULL,
+    `date` DATE,
+    `event` VARCHAR ( 255 ),
+    `price` VARCHAR ( 255 ),
+    `priceSqft` VARCHAR ( 255 ),
+    `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP 
+  )";
+
+    if ($db->query($priceHistorySql) === TRUE) {
+      echo "Table price_history created successfully \n";
+    } else {
+      echo "Error creating price_history table: " . $conn->error . "\n";
+    }
+  } else {
+    echo "Error dropping price_history table: " . $conn->error . "\n";
+  }
+
+  // check tax_history table
+  $dropTaxHistorySql = "DROP TABLE IF EXISTS tax_history";
+
+  if ($db->query($dropTaxHistorySql) === TRUE) {
+    $taxHistorySql = "CREATE TABLE IF NOT EXISTS tax_history (
+    `id` INT ( 6 ) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `zpid` VARCHAR ( 255 ) NOT NULL,
+    `year` INT ( 11 ),
+    `tax` VARCHAR ( 255 ),
+    `taxRate` VARCHAR ( 255 ),
+    `taxAssessment` VARCHAR ( 255 ),
+    `taxAssessmentRate` VARCHAR ( 255 ),
+    `createdAt` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP 
+  )";
+
+    if ($db->query($taxHistorySql) === TRUE) {
+      echo "Table tax_history created successfully \n";
+    } else {
+      echo "Error creating tax_history table: " . $conn->error . "\n";
+    }
+  } else {
+    echo "Error dropping tax_history table: " . $conn->error . "\n";
+  }
+
+  // check the selenium server
+  if (PHP_OS === "Linux") {
+    $serviceName = "selenium.service";
+    $checkCommand = "systemctl is-active $serviceName";
+    $output = shell_exec($checkCommand);
+
+    if (trim($output) !== "active") {
+      $startCommand = "sudo systemctl start $serviceName";
+      $startOutput = shell_exec($startCommand);
+
+      echo "Selenium Service was not running. Attempting to start...\n";
+      echo "Start command output: $startOutput\n";
+    }
+  }
+}
+
+function scrapeProperties($propertyElements)
+{
   global $db;
-  $value = array();
+  $result = array();
 
   if (count($propertyElements) > 0) {
     foreach ($propertyElements as $propertyElement) {
@@ -19,16 +136,392 @@ function scrapeProperties($propertyElements) {
         if ($exist->num_rows == 0) {
           $link = $propertyElement->findElement(WebDriverBy::cssSelector("div.property-card-data > a"))->getAttribute("href");
 
-          $value[] = array(
+          $result[] = array(
             "zpid" => $zpid,
             "link" => $link,
           );
         }
       }
     }
-    return $value;
-  } else {
-    return array();
   }
+
+  return $result;
 }
 
+function scrapePropertyDetail($detailHtml)
+{
+  global $db, $driver;
+
+  // get price
+  try {
+    $price = $detailHtml->findElement(WebDriverBy::cssSelector("div.summary-container span.Text-c11n-8-84-3__sc-aiai24-0.dpf__sc-1me8eh6-0.OByUh.fpfhCd > span"))->getText();
+  } catch (NoSuchElementException $e) {
+    $price = "";
+  }
+
+  // get address
+  try {
+    $address = $detailHtml->findElement(WebDriverBy::cssSelector("div.summary-container h1.Text-c11n-8-84-3__sc-aiai24-0.hrfydd"))->getText();
+  } catch (NoSuchElementException $e) {
+    $address = "";
+  }
+
+  // get beds
+  try {
+    $beds = $detailHtml->findElement(WebDriverBy::cssSelector("div.summary-container span[data-testid=\"bed-bath-item\"]:nth-child(1) strong"))->getText();
+  } catch (NoSuchElementException $e) {
+    $beds = "";
+  }
+
+  // get baths
+  try {
+    $baths = $detailHtml->findElement(WebDriverBy::cssSelector("div.summary-container button[class*=\"TriggerText\"] span[data-testid=\"bed-bath-item\"] strong"))->getText();
+  } catch (NoSuchElementException $e) {
+    $baths = "";
+  }
+
+  // get sqft
+  try {
+    $sqft = $detailHtml->findElement(WebDriverBy::cssSelector("div.summary-container span[data-testid=\"bed-bath-item\"]:nth-child(5) strong"))->getText();
+  } catch (NoSuchElementException $e) {
+    $sqft = "";
+  }
+
+  // get type
+  try {
+    $type = $detailHtml->findElement(WebDriverBy::cssSelector("div.hdp__sc-13r9t6h-0.ds-chip-removable-content span div.dpf__sc-1yftt2a-0.bNENJa span.Text-c11n-8-84-3__sc-aiai24-0.dpf__sc-1yftt2a-1.hrfydd.ixkFNb"))->getText();
+  } catch (NoSuchElementException $e) {
+    $type = "";
+  }
+
+  // get zestimate
+  try {
+    $zestimate = $detailHtml->findElement(WebDriverBy::cssSelector("div.hdp__sc-13r9t6h-0.ds-chip-removable-content span div.hdp__sc-j76ge-1.fomYLZ > span.Text-c11n-8-84-3__sc-aiai24-0.hrfydd > span.Text-c11n-8-84-3__sc-aiai24-0.hqOVzy span"))->getText();
+  } catch (NoSuchElementException $e) {
+    $zestimate = "None";
+  }
+
+  // get special info
+  try {
+    $special = $detailHtml->findElement(WebDriverBy::cssSelector("div.hdp__sc-ld4j6f-0.cKHmSE span.StyledTag-c11n-8-84-3__sc-1945joc-0.ftTUfk hdp__sc-ld4j6f-1.cosjzO"))->getText();
+  } catch (NoSuchElementException $e) {
+    $special = "";
+  }
+
+  // get overview
+  try {
+    $overview = $detailHtml->findElement(WebDriverBy::cssSelector("div.Text-c11n-8-84-3__sc-aiai24-0.sc-oZIhv.hrfydd.jKaobh"))->getText();
+  } catch (NoSuchElementException $e) {
+    $overview = "";
+  }
+
+  // get house info
+  $houseElements = $detailHtml->findElements(WebDriverBy::cssSelector("div.data-view-container ul.dpf__sc-xzpkxd-0.dFxsBL li.dpf__sc-2arhs5-0.gRshUo"));
+  $houseElementsResult = scrapeHouseElements($houseElements);
+
+  // get days, views, saves
+  $dtElements = $detailHtml->findElements(WebDriverBy::cssSelector("dl.hdp__sc-7d6bsa-0.gmVtvh dt"));
+  $dtElementsResult = scrapeDtElements($dtElements);
+
+  // get price history
+  $priceRowElements = $detailHtml->findElements(WebDriverBy::cssSelector("table.hdp__sc-f00yqe-2.jaEmHG tbody tr.hdp__sc-f00yqe-3.hTnieU"));
+  $priceHistory = scrapePriceHistory($priceRowElements);
+
+  // get tax history
+  $taxRowElements = $detailHtml->findElements(WebDriverBy::cssSelector("table.StyledTable-c11n-8-84-3__sc-b979s8-0.jtAqyI tbody tr.StyledTableRow-c11n-8-84-3__sc-1gk7etl-0.kaeLLi"));
+  $taxHistory = scrapeTaxHistory($taxRowElements);
+
+  return array(
+    "price" => $price,
+    "address" => $address,
+    "beds" => $beds,
+    "baths" => $baths,
+    "sqft" => $sqft,
+    "type" => $type,
+    "zestimate" => $zestimate,
+    "houseType" => $houseElementsResult["houseType"],
+    "builtYear" => $houseElementsResult["builtYear"],
+    "heating" => $houseElementsResult["heating"],
+    "cooling" => $houseElementsResult["cooling"],
+    "parking" => $houseElementsResult["parking"],
+    "lot" => $houseElementsResult["lot"],
+    "priceSqft" => $houseElementsResult["priceSqft"],
+    "agencyFee" => $houseElementsResult["agencyFee"],
+    "special" => $special,
+    "overview" => $overview,
+    "days" => $dtElementsResult["days"],
+    "views" => $dtElementsResult["views"],
+    "saves" => $dtElementsResult["saves"],
+    "priceHistory" => $priceHistory,
+    "taxHistory" => $taxHistory,
+  );
+}
+
+function scrapeHouseElements($houseElements)
+{
+  global $driver;
+
+  $houseType = "";
+  $builtYear = "";
+  $heating = "";
+  $cooling = "";
+  $parking = "";
+  $lot = "";
+  $priceSqft = "";
+  $agencyFee = "";
+
+  if (count($houseElements) > 0) {
+    foreach ($houseElements as $houseElement) {
+      try {
+        $svgElement = $houseElement->findElement(WebDriverBy::cssSelector("svg.Icon-c11n-8-84-3__sc-13llmml-0.iAcAav"));
+
+        $script = "return arguments[0].querySelector('title').textContent;";
+        $title = $driver->executeScript($script, [$svgElement]);
+
+        if ($title) {
+          switch ($title) {
+            case "Type":
+              try {
+                $houseType = $houseElement->findElement(WebDriverBy::cssSelector("span.Text-c11n-8-84-3__sc-aiai24-0.dpf__sc-2arhs5-3.hrfydd.kOlNqB"))->getText();
+              } catch (NoSuchElementException $e) {
+                $houseType = "";
+              }
+              break;
+            case "Year Built":
+              try {
+                $builtYear = $houseElement->findElement(WebDriverBy::cssSelector("span.Text-c11n-8-84-3__sc-aiai24-0.dpf__sc-2arhs5-3.hrfydd.kOlNqB"))->getText();
+              } catch (NoSuchElementException $e) {
+                $builtYear = "";
+              }
+              break;
+            case "Heating":
+              try {
+                $heating = $houseElement->findElement(WebDriverBy::cssSelector("span.Text-c11n-8-84-3__sc-aiai24-0.dpf__sc-2arhs5-3.hrfydd.kOlNqB"))->getText();
+              } catch (NoSuchElementException $e) {
+                $heating = "";
+              }
+              break;
+            case "Cooling":
+              try {
+                $cooling = $houseElement->findElement(WebDriverBy::cssSelector("span.Text-c11n-8-84-3__sc-aiai24-0.dpf__sc-2arhs5-3.hrfydd.kOlNqB"))->getText();
+              } catch (NoSuchElementException $e) {
+                $cooling = "";
+              }
+              break;
+            case "Parking":
+              try {
+                $parking = $houseElement->findElement(WebDriverBy::cssSelector("span.Text-c11n-8-84-3__sc-aiai24-0.dpf__sc-2arhs5-3.hrfydd.kOlNqB"))->getText();
+              } catch (NoSuchElementException $e) {
+                $parking = "";
+              }
+              break;
+            case "Lot":
+              try {
+                $lot = $houseElement->findElement(WebDriverBy::cssSelector("span.Text-c11n-8-84-3__sc-aiai24-0.dpf__sc-2arhs5-3.hrfydd.kOlNqB"))->getText();
+              } catch (NoSuchElementException $e) {
+                $lot = "";
+              }
+              break;
+            case "Price/sqft":
+              try {
+                $priceSqft = $houseElement->findElement(WebDriverBy::cssSelector("span.Text-c11n-8-84-3__sc-aiai24-0.dpf__sc-2arhs5-3.hrfydd.kOlNqB"))->getText();
+              } catch (NoSuchElementException $e) {
+                $priceSqft = "";
+              }
+              break;
+            case "Buyers Agency Fee":
+              try {
+                $agencyFee = $houseElement->findElement(WebDriverBy::cssSelector("span.Text-c11n-8-84-3__sc-aiai24-0.dpf__sc-2arhs5-3.hrfydd.kOlNqB"))->getText();
+              } catch (NoSuchElementException $e) {
+                $agencyFee = "";
+              }
+              break;
+          }
+        }
+      } catch (NoSuchElementException $e) {
+      }
+    }
+  }
+
+  return array(
+    "houseType" => $houseType,
+    "builtYear" => $builtYear,
+    "heating" => $heating,
+    "cooling" => $cooling,
+    "parking" => $parking,
+    "lot" => $lot,
+    "priceSqft" => $priceSqft,
+    "agencyFee" => $agencyFee,
+  );
+}
+
+function scrapeDtElements($dtElements)
+{
+  $days = "";
+  $views = "";
+  $saves = "";
+
+  if (count($dtElements) > 0) {
+    foreach ($dtElements as $key => $dtElement) {
+      switch ($key) {
+        case 0:
+          try {
+            $days = $dtElement->findElement(WebDriverBy::cssSelector("strong"))->getText();
+          } catch (NoSuchElementException $e) {
+            $days = "";
+          }
+          break;
+        case 1:
+          try {
+            $views = $dtElement->findElement(WebDriverBy::cssSelector("strong"))->getText();
+          } catch (NoSuchElementException $e) {
+            $views = "";
+          }
+          break;
+        case 2:
+          try {
+            $saves = $dtElement->findElement(WebDriverBy::cssSelector("strong"))->getText();
+          } catch (NoSuchElementException $e) {
+            $saves = "";
+          }
+          break;
+      }
+    }
+  }
+
+  return array(
+    "days" => $days,
+    "views" => $views,
+    "saves" => $saves,
+  );
+}
+
+function scrapePriceHistory($priceRowElements)
+{
+  $result = array();
+
+  if (count($priceRowElements) > 0) {
+    foreach ($priceRowElements as $priceRowElement) {
+      $date = "";
+      $event = "";
+      $priceItem = "";
+      $pricePerSqft = "";
+
+      $priceColumnElements = $priceRowElement->findElements(WebDriverBy::cssSelector("td"));
+      if (count($priceColumnElements) > 0) {
+        foreach ($priceColumnElements as $key => $priceColumnElement) {
+          switch ($key) {
+            case 0:
+              try {
+                $date = $priceColumnElement->findElement(WebDriverBy::cssSelector("span.hdp__sc-reo5z7-1.bRcAjm"))->getText();
+              } catch (NoSuchElementException $e) {
+                $date = "";
+              }
+              break;
+            case 1:
+              try {
+                $event = $priceColumnElement->findElement(WebDriverBy::cssSelector("span.hdp__sc-reo5z7-1.hdp__sc-reo5z7-4.bRcAjm.fTyeIS"))->getText();
+              } catch (NoSuchElementException $e) {
+                $event = "";
+              }
+              break;
+            case 2:
+              try {
+                $priceItem = $priceColumnElement->findElement(WebDriverBy::cssSelector("span.hdp__sc-reo5z7-1.hdp__sc-reo5z7-6.dQBikw.epSCRt span.hdp__sc-reo5z7-1.hdp__sc-reo5z7-5.bRcAjm.ldMXqX"))->getText();
+              } catch (NoSuchElementException $e) {
+                $priceItem = "";
+              }
+
+              try {
+                $pricePerSqft = $priceColumnElement->findElement(WebDriverBy::cssSelector("span.hdp__sc-reo5z7-1.hdp__sc-reo5z7-7.fEaJVg.jNzJBc"))->getText();
+              } catch (NoSuchElementException $e) {
+                $pricePerSqft = "";
+              }
+
+              break;
+          }
+          try {
+          } catch (NoSuchElementException $e) {
+          }
+        }
+      }
+      $result[] = array(
+        "date" => $date,
+        "event" => $event,
+        "price" => $priceItem,
+        "priceSqft" => $pricePerSqft,
+      );
+    }
+  }
+
+  return $result;
+}
+
+function scrapeTaxHistory($taxRowElements)
+{
+  $result = array();
+
+  if (count($taxRowElements) > 0) {
+    foreach ($taxRowElements as $taxRowElement) {
+      $year = 0;
+      $propertyTax = "";
+      $propertyTaxRate = "";
+      $taxAssessment = "";
+      $taxAssessmentRate = "";
+
+      try {
+        $year = $taxRowElement->findElement(WebDriverBy::cssSelector("th.StyledTableCell-c11n-8-84-3__sc-1mvjdio-0.StyledTableHeaderCell-c11n-8-84-3__sc-j48v56-0.eeNqSO span.hdp__sc-reo5z7-1.bRcAjm"))->getText();
+        $year = intval($year);
+      } catch (NoSuchElementException $e) {
+        $year = 0;
+      }
+
+      $taxColumnElements = $taxRowElement->findElements(WebDriverBy::cssSelector("td"));
+
+      if (count($taxColumnElements) > 0) {
+        foreach ($taxColumnElements as $key => $taxColumnElement) {
+          switch ($key) {
+            case 0:
+              try {
+                $propertyTax = $taxColumnElement->findElement(WebDriverBy::cssSelector("span.hdp__sc-reo5z7-1.bRcAjm"))->getText();
+              } catch (NoSuchElementException $e) {
+                $propertyTax = "";
+              }
+
+              try {
+                $propertyTaxRate = $taxColumnElement->findElement(WebDriverBy::cssSelector("span.hdp__sc-reo5z7-1.bRcAjm span.hdp__sc-vcntbl-0.frFkpC"))->getText();
+              } catch (NoSuchElementException $e) {
+                $propertyTaxRate = "";
+              }
+
+              break;
+            case 1:
+              try {
+                $taxAssessment = $taxColumnElement->findElement(WebDriverBy::cssSelector("span.hdp__sc-reo5z7-1.bRcAjm"))->getText();
+              } catch (NoSuchElementException $e) {
+                $taxAssessment = "";
+              }
+
+              try {
+                $taxAssessmentRate = $taxColumnElement->findElement(WebDriverBy::cssSelector("span.hdp__sc-reo5z7-1.bRcAjm span.hdp__sc-vcntbl-0.frFkpC"))->getText();
+              } catch (NoSuchElementException $e) {
+                $taxAssessmentRate = "";
+              }
+
+              break;
+          }
+        }
+      }
+
+      $result[] = array(
+        "year" => $year,
+        "tax" => $propertyTax,
+        "taxRate" => $propertyTaxRate,
+        "taxAssessment" => $taxAssessment,
+        "taxAssessmentRate" => $taxAssessmentRate,
+      );
+    }
+  }
+
+  return $result;
+}

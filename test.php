4043b062562dec7bb3a5b8b4c4f32ca6c7e89fb7
bendik0329 <razvanbendik0329@gmail.com>
2023-09-04 20:47:48 +0300

    update script

--- test.php
+++ test.php
@@ -1,15 +1,43 @@
 <?php
-require_once('vendor/autoload.php');
-require_once  __DIR__ . '/utils/constants.php';
 
-use Facebook\WebDriver\Remote\DesiredCapabilities;
+require_once __DIR__ . '/vendor/autoload.php';
+require_once  __DIR__ . '/utils/constants.php';
+require_once  __DIR__ . '/utils/database.php';
+require_once  __DIR__ . '/utils/scraping.php';
+
 use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\Exception\NoSuchElementException;
+use Facebook\WebDriver\WebDriver;
+use Facebook\WebDriver\WebDriverKeys;
+use voku\helper\HtmlDomParser;
+use Facebook\WebDriver\WebDriverDimension;
+use Facebook\WebDriver\WebDriverWait;
+use Facebook\WebDriver\WebDriverExpectedCondition;
+
+// load environment variable
+$envConfig = parse_ini_file(__DIR__ . "/.env");
+
+$host = $envConfig['DB_HOST'];
+$username = $envConfig['DB_USERNAME'];
+$password = $envConfig['DB_PASSWORD'];
+$dbname = $envConfig['DB_DATABASE'];
+$apiKey = $envConfig['API_KEY'];
+
+// Connect to DB
+$db  = new Database();
+if (!$db->connect($host, $username, $password, $dbname)) {
+  die("DB Connection failed: " . $conn->connect_error);
+}
+
+// initialize
+_init();
 
 $numParallel = 5;
 $pids = [];
 $chunks = array_chunk(STATE_LIST, $numParallel);
-
-print_r($chunks);
+$properties = [];
+$total = 0;
 
 foreach ($chunks as $chunk) {
   $pid = pcntl_fork();

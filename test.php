f2c6c67031800b9d5bc5fe43c1ebfa4b03d4cd19
bendik0329 <razvanbendik0329@gmail.com>
2023-09-04 20:04:51 +0300

    update script

--- test.php
+++ test.php
@@ -1,35 +1,9 @@
 <?php
-require 'vendor/autoload.php';
+require_once('vendor/autoload.php');
 
-use parallel\Runtime;
+$numParallel = 5;
+$states = array_chunk(STATE_LIST, $numParallel);
 
-$states = ['state1', 'state2', 'state3', 'state4', 'state5', 'state6', 'state7', 'state8', 'state9', 'state10'];
-$batchSize = 5; // Number of states to process simultaneously
+print_r($states);
 
-$runtime = new Runtime();
-
-$chunks = array_chunk($states, $batchSize); // Divide the states into chunks
-
-$runtime->run(function () use ($chunks) {
-  foreach ($chunks as $chunk) {
-    $threads = [];
-    foreach ($chunk as $state) {
-      $thread = new \parallel\Thread('scrapeProperties', [$state]);
-      $threads[] = $thread;
-      $thread->start();
-    }
-
-    foreach ($threads as $thread) {
-      $thread->join();
-    }
-  }
-});
-
-function scrapeProperties($state)
-{
-  // Your scraping logic for a single state goes here
-  // ...
-  echo "Scraping properties for $state\n";
-  sleep(5); // Simulating scraping process
-  echo "Finished scraping properties for $state\n";
-}
+$pid = pcntl_fork();

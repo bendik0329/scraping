2b8e3f0101380b6642f92f7b9a9d13d008811ae3
bendik0329 <razvanbendik0329@gmail.com>
2023-09-05 19:42:28 +0300

    update script

--- test.php
+++ test.php
@@ -10,46 +10,48 @@ use voku\helper\HtmlDomParser;
 $envConfig = parse_ini_file(__DIR__ . "/.env");
 $apiKey = $envConfig['API_KEY'];
 
+for ($i = 0; $i <= 100; $i++) {
+  $url = 'https://www.zillow.com/homedetails/506-E-Philadelphia-St-Rapid-City-SD-57701/117808623_zpid/';
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_PROXY, 'http://60cb4030eb5826662d404f6cb6bb10040a3f775a:@proxy.zenrows.com:8001');
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  curl_setopt($ch, CURLOPT_USERAGENT, USER_AGENT);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+  $html = curl_exec($ch);
+  curl_close($ch);
 
-$url = 'https://www.zillow.com/homedetails/506-E-Philadelphia-St-Rapid-City-SD-57701/117808623_zpid/';
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_PROXY, 'http://60cb4030eb5826662d404f6cb6bb10040a3f775a:@proxy.zenrows.com:8001');
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt($ch, CURLOPT_USERAGENT, USER_AGENT);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-$html = curl_exec($ch);
-curl_close($ch);
+  $htmlDomParser = HtmlDomParser::str_get_html($html);
+  $detailHtml = $htmlDomParser->findOne("div.detail-page");
 
-$htmlDomParser = HtmlDomParser::str_get_html($html);
-$detailHtml = $htmlDomParser->findOne("div.detail-page");
+  // get price
+  $priceElement = $detailHtml->findOne("div.summary-container div.hdp__sc-1s2b8ok-1.ckVIjE span.Text-c11n-8-84-3__sc-aiai24-0.dpf__sc-1me8eh6-0.OByUh.fpfhCd > span");
+  if ($priceElement instanceof \voku\helper\SimpleHtmlDomBlank) {
+    $price = 0;
+  } else {
+    $priceText = $priceElement->text();
+    $price = deformatPrice($priceText);
+  }
 
-// get price
-$priceElement = $detailHtml->findOne("div.summary-container div.hdp__sc-1s2b8ok-1.ckVIjE span.Text-c11n-8-84-3__sc-aiai24-0.dpf__sc-1me8eh6-0.OByUh.fpfhCd > span");
-if ($priceElement instanceof \voku\helper\SimpleHtmlDomBlank) {
-  $price = 0;
-} else {
-  $priceText = $priceElement->text();
-  $price = deformatPrice($priceText);
+
+  // get overview
+  $overviewElement = $detailHtml->findOne("div.Text-c11n-8-84-3__sc-aiai24-0.sc-oZIhv.hrfydd");
+  if ($overviewElement instanceof \voku\helper\SimpleHtmlDomBlank) {
+    $overview = "";
+  } else {
+    $overview = $overviewElement->text();
+  }
+
+  print_r("price->>" . $price);
+  print_r("\n");
+
+  print_r("overview->>" . $overview);
+  print_r("\n");
 }
 
-
-// get overview
-$overviewElement = $detailHtml->findOne("div.Text-c11n-8-84-3__sc-aiai24-0.sc-oZIhv.hrfydd");
-if ($overviewElement instanceof \voku\helper\SimpleHtmlDomBlank) {
-  $overview = "";
-} else {
-  $overview = $overviewElement->text();
-}
-
-print_r("price->>" . $price);
-print_r("\n");
-
-print_r("overview->>" . $overview);
-print_r("\n");
 // print_r("overview->>" . $overview);
 // print_r("\n");
 exit();

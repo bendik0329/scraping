21c773e53c01b26a5a9af9bebf37a8af0cd6f21b
bendik0329 <razvanbendik0329@gmail.com>
2023-09-09 16:08:41 +0300

    update script

--- scraping2.php
+++ scraping2.php
@@ -91,8 +91,6 @@ function scrape($db)
         $searchQueryState = urlencode($queryString);
 
         $url = "https://api.scrapingdog.com/scrape?api_key=$apiKey&url=https://www.zillow.com/$stateAlias/?searchQueryState=$searchQueryState&dynamic=false";
-
-        echo $url . "\n";
         
         $driver->get($url);
 

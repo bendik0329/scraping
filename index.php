f950baf007ea0d0bc2eecd4f5d086350ea8d1443
bendik0329 <razvanbendik0329@gmail.com>
2023-09-13 15:08:26 +0800

    update script

--- index.php
+++ index.php
@@ -412,7 +412,7 @@ function scrapeProperties($driver, $db, $count, $state, $type, $category, $range
 }
 
 // Divide states into batches of 5
-$stateBatches = array_chunk(STATE_LIST, 3);
+$stateBatches = array_chunk(STATE_LIST, 5);
 
 // Get the batch to scrape based on the startIndex
 $batchToScrape = isset($stateBatches[$startIndex]) ? $stateBatches[$startIndex] : [];

4699a8fcf23cfbe8279059b4527a99f94b2bd680
bendik0329 <razvanbendik0329@gmail.com>
2023-09-14 06:40:55 +0800

    update

--- run_parallel.sh
+++ run_parallel.sh
@@ -2,11 +2,11 @@
 
 # Run init file
 echo "Initialize Database..."
-php init.php
+# php init.php
 
 # Run 10 parallel instances
 echo "Running 10 parallel scraping instances..."
-for i in {0..9}; do
+for i in {0..0}; do
   php index.php $i &
 done
 
@@ -16,4 +16,4 @@ wait
 
 # Download Images
 echo "Downloading Images..."
-php downloads.php
+# php downloads.php
